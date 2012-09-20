f7938a10089fafcd1e2374889c4b0b37ce6dae81
cloverrose <clover.plus.rose@gmail.com>
2012-09-20 10:42:02 +0900

    first test

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main():
+    """
+    >>> 1 + 1
+    2
+    """
+    pass
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
